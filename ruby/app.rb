2a599340bb7501fe744559f6d61c5acdd59b2133
Carolina Bonilla <bonillacarolina165@gmail.com>
2026-06-24 18:24:08 -0600

    ruby v3: Conversion nativa de numero a letras en espanol usando numbers_and_words

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,34 +1,26 @@
 require 'sinatra'
-require 'savon'
-require 'net/http'
-require 'json'
-require 'uri'
+require 'numbers_and_words'
 
 set :port, 5000
+
+# Configurar el idioma por defecto de la librería a español
+I18n.enforce_available_locales = false
+I18n.locale = :es
 
 get '/' do
   numero = params['n']
   
-  if numero.nil? || numero.empty?
-    return "Por favor, proporciona un número válido en la URL. Ejemplo: http://localhost:5000/?n=10"
+  # Validamos que el parámetro exista y sea un número entero válido
+  if numero.nil? || numero.empty? || numero !~ /\A\d+\z/
+    return "Por favor, proporciona un número entero válido en la URL. Ejemplo: http://localhost:5000/?n=10"
   end
 
   begin
-    # 1. Consumir el servicio SOAP
-    client = Savon.client(wsdl: "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL")
-    response = client.call(:number_to_words, message: { ubiNum: numero })
-    resultado_ingles = response.body[:number_to_words_response][:number_to_words_result].to_s.strip
-
-    # 2. Traducir usando la API de MyMemory de Inglés a Español
-    url_texto = URI.encode_www_form_component(resultado_ingles)
-    uri = URI("https://api.mymemory.translated.net/get?q=#{url_texto}&langpair=en|es")
+    # Convertimos la cadena a un entero y luego a letras en español
+    resultado_letras = numero.to_i.to_words
     
-    res = Net::HTTP.get(uri)
-    datos = JSON.parse(res)
-    resultado_espanol = datos["responseData"]["translatedText"]
-    
-    resultado_espanol.downcase
+    resultado_letras.downcase
   rescue => e
-    "Error en el proceso: #{e.message}"
+    "Error en la conversión: #{e.message}"
   end
-end
+end 
